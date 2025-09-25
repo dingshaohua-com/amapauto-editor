c9c84a5ece5f8149fe5f28475174e0f1790eb028
丁少华 <960423114@qq.com>
2025-09-25 23:35:06 +0800

    fix

--- src/renderer/src/components/step-two.tsx
+++ src/renderer/src/components/step-two.tsx
@@ -1,20 +1,5 @@
-import { useAppCtx } from '@renderer/hooks/use-app-ctx'
-export default function StepTwo(): React.JSX.Element {
-  const { unPackPath } = useAppCtx()
-  
-  const onClick = ()=>{
-    electron.ipcRenderer.invoke('build-apk', unPackPath)
-  }
-  
-  return (
-    <div className="flex flex-col">
-      <div>解包路径：{unPackPath}</div>
-         <div
-            className="bg-blue-400 rounded p-2 cursor-pointer w-40 text-center"
-            onClick={onClick}
-          >
-            重新生成APK
-          </div>
-    </div>
-  )
+import { Button } from "@renderer/components/ui/button"
+
+export default function Demo() {
+  return  <Button>Button</Button>
 }
