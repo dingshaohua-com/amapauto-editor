91ec04fc72826878b31f60a648f82bb7d4f2cee7
丁少华 <960423114@qq.com>
2025-09-26 01:08:08 +0800

    fix

--- src/renderer/src/components/step-two.tsx
+++ src/renderer/src/components/step-two.tsx
@@ -1,13 +1,5 @@
 import { useAppCtx } from '@renderer/hooks/use-app-ctx'
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue
-} from '@renderer/components/ui/select'
+import { Combobox } from '@renderer/components/ui/combobox'
 
 export default function StepTwo(): React.JSX.Element {
   const { unPackPath } = useAppCtx()
@@ -19,19 +11,11 @@ export default function StepTwo(): React.JSX.Element {
   return (
     <div className="flex flex-col text-white">
       <div>解包路径：{unPackPath}</div>
-      <div>设置包名：<Select>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="Select a fruit" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Fruits</SelectLabel>
-            <SelectItem value="apple">Apple</SelectItem>
-            <SelectItem value="banana">Banana</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select></div>
-      
+      <div>
+        设置包名：
+        <Combobox />
+      </div>
+
       <div className="bg-blue-400 rounded p-2 cursor-pointer w-40 text-center" onClick={onClick}>
         重新生成APK
       </div>
