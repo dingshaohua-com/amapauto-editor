c09659da54535c7a7c75736422151ea53ace3b85
丁少华 <960423114@qq.com>
2025-10-12 12:58:59 +0800

    fix

--- src/renderer/src/pages/home/helper.tsx
+++ src/renderer/src/pages/home/helper.tsx
@@ -1,10 +1,14 @@
-import { Package, FileText, Smartphone, Code, Shield, Zap, Database, Globe } from 'lucide-react';
+import editAppImg from '@renderer/assets/imgs/edit-app.svg';
+import packAppImg from '@renderer/assets/imgs/pack-app.svg';
+import signAppImg from '@renderer/assets/imgs/sign-app.svg';
+import patchAppImg from '@renderer/assets/imgs/patch-app.svg';
+import unpackAppImg from '@renderer/assets/imgs/unpack-app.svg';
 
 export const getMenuItems = (navigate) => {
   return [
     {
-      icon: <Package className="w-6 h-6" />,
-      title: 'APK 解包',
+      icon: <img className="w-7 h-7" src={unpackAppImg} />,
+      title: '解包',
       description: '解析 APK 文件结构',
       variant: 'primary' as const,
       onClick: () => {
@@ -12,57 +16,60 @@ export const getMenuItems = (navigate) => {
       },
     },
     {
-      icon: <Code className="w-6 h-6" />,
-      title: '编辑包',
-      description: '修改APP信息',
-      variant: 'pink' as const,
-      onClick: () => {
-        navigate('/edit-app');
-      },
-    },
-    {
-      icon: <Shield className="w-6 h-6" />,
-      title: '签名工具',
-      description: '为 APK 添加数字签名',
-      onClick: () => {
-        navigate('/sign-app');
-      },
-    },
-    {
-      icon: <Zap className="w-6 h-6" />,
-      title: '性能优化',
-      description: '优化应用性能',
+      icon: <img className="w-8 h-8" src={patchAppImg} />,
+      title: '修补',
+      description: '针对打包失败或优化',
+      variant: 'secondary' as const,
       onClick: () => {
         navigate('/patch-app');
       },
     },
     {
-      icon: <Smartphone className="w-6 h-6" />,
-      title: 'APK 重打包',
+      icon: <img className="w-7 h-7" src={editAppImg} />,
+      title: '编辑包',
+      description: '修改APP信息',
+      variant: 'danger' as const,
+      onClick: () => {
+        navigate('/edit-app');
+      },
+    },
+    {
+      icon: <img className="w-8 h-8" src={signAppImg} />,
+      title: '签名',
+      description: '为 APK 添加数字签名',
+      variant: 'warning' as const,
+      onClick: () => {
+        navigate('/sign-app');
+      },
+    },
+
+    {
+      icon: <img className="w-7 h-7" src={packAppImg} />,
+      title: '重打包',
       description: '重新生成 APK 文件',
-      variant: 'secondary' as const,
+      variant: 'info' as const,
       onClick: () => console.log('APK 重打包'),
     },
 
-    {
-      icon: <FileText className="w-6 h-6" />,
-      title: '资源管理',
-      description: '管理应用资源文件',
-      onClick: () => console.log('资源管理'),
-    },
+    // {
+    //   icon: <FileText className="w-6 h-6" />,
+    //   title: '资源管理',
+    //   description: '管理应用资源文件',
+    //   onClick: () => console.log('资源管理'),
+    // },
 
-    {
-      icon: <Database className="w-6 h-6" />,
-      title: '数据库工具',
-      description: '管理应用数据库',
-      onClick: () => console.log('数据库工具'),
-    },
-    {
-      icon: <Globe className="w-6 h-6" />,
-      title: '网络调试',
-      description: '网络请求分析',
-      onClick: () => console.log('网络调试'),
-    },
+    // {
+    //   icon: <Database className="w-6 h-6" />,
+    //   title: '数据库工具',
+    //   description: '管理应用数据库',
+    //   onClick: () => console.log('数据库工具'),
+    // },
+    // {
+    //   icon: <Globe className="w-6 h-6" />,
+    //   title: '网络调试',
+    //   description: '网络请求分析',
+    //   onClick: () => console.log('网络调试'),
+    // },
   ];
 };
 
